2997c0530531e4f85c266f473efa56aba7bbb6a8
Evseev Artem <Luzifer_Morningstar@mail.ru>
2020-12-15 22:44:15 +0300

    Upload lab #3

---
+++ Lab3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class ZeroDivisionException{};
+int operation(int a, int b, char op);
+int main()
+{
+    
+    string expression;
+    stack<int> operands;
+    int number = 0;
+    bool flag = true; //флаг для считывания многозначных чисел
+
+    cout << "enter expression: ";
+    getline(cin, expression);
+
+    for (char symbol : expression)
+    {
+        if (isdigit(symbol))
+        {
+            number *= 10;
+            number += (symbol - '0');
+            flag = true;
+        }
+        else
+        {
+            if (symbol != ' ')
+            {
+                int num2 = operands.top();
+                operands.pop();
+                int num1 = operands.top();
+                operands.pop();
+                try 
+                {
+                    operands.push(operation(num1, num2, symbol));
+                }
+                catch (ZeroDivisionException)
+                {
+                    cout << "There is division by zero in your expression!";
+                    return 0;
+                }
+                flag = false; //если пробел идет после оператора, ветвь ниже не выполняется
+            }
+            else if (symbol == ' ' && flag)//если пробел идет после числа, оно заносится в стек
+            {
+                operands.push(number);
+                number = 0;
+            }
+        }
+    }
+
+    cout << operands.top();
+
+    return 0;
+}
+
+int operation(int a, int b, char op)
+{
+    switch (op) 
+    {
+        case '+':return a + b;
+        case '-':return a - b;
+        case '*':return a * b;
+        case '/':
+            if (b == 0)throw ZeroDivisionException();
+            else return a / b;
+    }
+}
